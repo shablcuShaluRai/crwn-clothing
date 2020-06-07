6a87f0de4e206f9778bf055402aecc583582bd44
shablcuShaluRai <shaluraics22@gmail.com>
2020-06-07 19:54:35 +0530

    Added homepage componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Homepage from "./Homepage"
 
 function App() {
   return (
     <div className="App">
-       Hello World!
+       <Homepage />
     </div>
   );
 }
