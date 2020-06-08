d31307296d41dfb18d8cff3b4e18b5487f7291b9
shablcuShaluRai <shaluraics22@gmail.com>
2020-06-08 22:23:28 +0530

    Added basic route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Route } from "react-router-dom"
 import Homepage from "./views/Homepage"
+
+
+function HatsPage() {
+  return(
+    <div>Hats PAgE</div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-       <Homepage />
+      <Route exact path="/" component={Homepage} />
+      <Route path="/hats" component={HatsPage} />
     </div>
   );
 }
